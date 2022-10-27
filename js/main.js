59cc7b8a9b04808ad06269f61aa134acbb02da20
Aiiishaaa <aicha.hamida06@yahoo.fr>
2022-10-27 20:27:32 +0200

    update scss deleted

--- js/main.js
+++ js/main.js
@@ -9,43 +9,23 @@
 
 'use strict';
 
-(function ($) {
-
-    /*------------------
-        Preloader
-    --------------------*/
-    $(window).on('load', function () {
-        $(".loader").fadeOut();
-        $("#preloder").delay(200).fadeOut("slow");
-
-        /*------------------
-            FIlter
-        --------------------*/
-        $('.filter__controls li').on('click', function () {
-            $('.filter__controls li').removeClass('active');
-            $(this).addClass('active');
-        });
-        if ($('.filter__gallery').length > 0) {
-            var containerEl = document.querySelector('.filter__gallery');
-            var mixer = mixitup(containerEl);
-        }
-    });
+(function($) {
 
     /*------------------
         Background Set
     --------------------*/
-    $('.set-bg').each(function () {
+    $('.set-bg').each(function() {
         var bg = $(this).data('setbg');
         $(this).css('background-image', 'url(' + bg + ')');
     });
 
     // Search model
-    $('.search-switch').on('click', function () {
+    $('.search-switch').on('click', function() {
         $('.search-model').fadeIn(400);
     });
 
-    $('.search-close-switch').on('click', function () {
-        $('.search-model').fadeOut(400, function () {
+    $('.search-close-switch').on('click', function() {
+        $('.search-model').fadeOut(400, function() {
             $('#search-input').val('');
         });
     });
@@ -96,6 +76,6 @@
     $("#scrollToTopButton").click(function() {
         $("html, body").animate({ scrollTop: 0 }, "slow");
         return false;
-     });
+    });
 
 })(jQuery);
